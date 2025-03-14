41ab350e9f5f8774807d4c60be2f6961d7895438
Max Nagornyi <max.nagornyi@goodahead.com>
2025-03-14 14:42:08 +0200

    Added book id

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/books/actionCreators';
+import { v4 as uuidv4 } from 'uuid';
 import './BookForm.css';
 
 const BookForm = () => {
@@ -15,6 +16,7 @@ const BookForm = () => {
       const book = {
         title: title,
         author: author,
+        id: uuidv4(),
       };
 
       dispatch(addBook(book));
